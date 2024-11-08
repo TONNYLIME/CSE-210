e21b2f4b7b65da9df5070b773e143a143db268c8
tonnylumwaji <>
2024-11-08 21:42:31 +0300

    I have finished prep3.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,34 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
-class Program
+// ask the user for the magic number
+Console.Write("What is the magic number? ");
+// convert the input from the user to and integer
+int magicNumber = int.Parse(Console.ReadLine());
+//Console.WriteLine(magicNumber);
+
+// ask the user for a random number to guess the margic number
+Random randomGenerator = new Random();
+int number = randomGenerator.Next(1, 101);
+
+int guess = -1;
+while (guess != magicNumber)
 {
-    static void Main(string[] args)
+    Console.Write("What is your guess? ");
+    guess = int.Parse(Console.ReadLine());
+
+    if (magicNumber > guess)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("Higher");
+    }
+
+    else if (magicNumber < guess)
+    {
+        Console.WriteLine("Lower");
+    }
+
+    else
+    {
+        Console.WriteLine("You have guessed it!");
     }
 }

160b70552c8a4f683dc4afcf0ff1c30a977fe073
tonnylumwaji <>
2024-11-16 14:28:43 +0300

    lesson two job and resume entry

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job ();
+        job1._jobTitle = "Electrical Technician";
+        job1._company = "Subaru Kenya";
+        job1._startYear = 2021;
+        job1._endYear = 2025;
+        job1.Display();
+
+        Job job2 = new Job ();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Microsoft";
+        job2._startYear = 2025;
+        job2._endYear = 2028;
+        job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "Tonny L.Lime";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+        
+
     }
 }

82079c8411c425d74637fb48c299234c432975b2
tonnylumwaji <>
2024-12-17 22:22:32 +0300

    repeat of the first submition

--- foundation/Foundation3/Program.cs
+++ foundation/Foundation3/Program.cs
@@ -4,7 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Create instances of each activity
+        // Create instances of each activity 
         Activity running = new Running(new DateTime(2024, 12, 17), 40, 3.0); // 40 min, 3 miles
         Activity cycling = new Cycling(new DateTime(2024, 12, 17), 30, 20.0); // 30 min, 20 mph
         Activity swimming = new Swimming(new DateTime(2024, 12, 7), 10, 25); // 10 min, 25 laps
